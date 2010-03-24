f0a4e9b4baa5bda4e1d820b763117ff9bf54e04d
Paul Craciunoiu <paul.craciunoiu@gmail.com>
2010-03-24 11:36:14 -0700

    initial kohana checkin. hello world

---
+++ application/logs/2010/03/23.php
@@ -0,0 +1,5 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2010-03-23 02:03:17 --- ERROR: ReflectionException [ -1 ]: Class controller_public_html does not exist ~ SYSPATH/classes/kohana/request.php [ 845 ]
+2010-03-23 02:03:19 --- ERROR: ReflectionException [ -1 ]: Class controller_public_html does not exist ~ SYSPATH/classes/kohana/request.php [ 845 ]
+2010-03-23 02:03:27 --- ERROR: ReflectionException [ -1 ]: Class controller_public_html does not exist ~ SYSPATH/classes/kohana/request.php [ 845 ]
