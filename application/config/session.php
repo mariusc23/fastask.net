266854e1eb11249e1ccbe5111b1d661e45eb7959
Paul Craciunoiu <paul.craciunoiu@gmail.com>
2010-04-03 19:17:55 -0700

    session encrypted and lifetime added

---
+++ application/config/session.php
@@ -0,0 +1,14 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array(
+    'native' => array(
+        'encrypted' => TRUE,
+        'name'   => 'tasklistv1',
+        'lifetime' => 1209600,
+    ),
+    'cookie' => array(
+        'name'   => 'tasklistv1',
+        'encrypted' => TRUE,
+        'lifetime' => 1209600,
+    ),
+);
