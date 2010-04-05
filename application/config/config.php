55c002d5364c7c303029839983970323ea431047
Paul Craciunoiu (Linode) <paul@craciunoiu.net>
2010-04-05 04:51:03 +0000

    Making this play nice with quoteden's sphinx conf

--- application/config/config.php
+++ application/config/config.php
@@ -13,7 +13,7 @@ define('SPHINX_RANKER', 0);
 define('SPHINX_INDEX', 'tasklist');
 // set host, port to access sphinxd
 define('SPHINX_HOST', 'localhost');
-define('SPHINX_PORT', 3312);
+define('SPHINX_PORT', 3311);
 
 // pagination
 define('PAGES_BEFORE', 5);
