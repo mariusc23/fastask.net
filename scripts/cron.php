7802606957c9365c31b4b7059c083947d06a372d
Paul Craciunoiu <paul.craciunoiu@gmail.com>
2010-04-05 18:35:19 -0700

    Adding cron

---
+++ scripts/cron.php
@@ -0,0 +1,33 @@
+<?php
+chdir(dirname(__FILE__));
+
+require_once('config.php');
+
+try {
+if (defined('CRON_REPEAT_INTERVAL')) {
+    $interval = CRON_REPEAT_INTERVAL;
+} else {
+    $interval = 'INTERVAL 30 DAY';
+}
+
+$db_link = new PDO("mysql:host=" . DB_KOHANA_HOST . ";dbname=" . DB_KOHANA_NAME, DB_KOHANA_USER, DB_KOHANA_PASS);
+
+$q = "
+    DELETE FROM
+        tasks
+    WHERE
+        trash = 1
+    AND lastmodified < DATE_SUB(CURRENT_DATE(), $interval)
+    ;";
+$statement = $db_link->prepare($q);
+$count = $statement->execute();
+
+if ($count) {
+    $row_count = intval($statement->rowCount());
+    echo "Cron ran successfully. $row_count tasks erased.\n";
+} else {
+    echo "No tasks erased.\n";
+}
+} catch (PDOException $e) {
+    die($e->getMessage());
+}
